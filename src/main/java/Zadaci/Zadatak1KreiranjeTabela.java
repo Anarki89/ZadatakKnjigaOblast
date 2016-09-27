bda200d97a47de3063e6690237abf8b4eb7d5f58
Pedja <anarki@gmail.com>
2016-09-27 19:21:44 +0200

    Dodato kreiranje tabela! fixes #3

--- src/main/java/Zadaci/Zadatak1KreiranjeTabela.java
+++ src/main/java/Zadaci/Zadatak1KreiranjeTabela.java
@@ -1,10 +1,42 @@
 package Zadaci;
 
+import Model.Knjiga;
+import Model.Oblast;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.io.IOException;
+
 /**
  * Created by android on 27.9.16..
  */
 public class Zadatak1KreiranjeTabela {
     public static void main(String[] args) {
+
+        ConnectionSource connectionSource = null;
+        try {
+
+            connectionSource = new JdbcConnectionSource(konstante.DATABASE_URL);
+
+            TableUtils.dropTable(connectionSource, Oblast.class,true);
+            TableUtils.dropTable(connectionSource, Knjiga.class,true);
+
+            TableUtils.createTable(connectionSource,Oblast.class);
+            TableUtils.createTable(connectionSource,Knjiga.class);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 }
